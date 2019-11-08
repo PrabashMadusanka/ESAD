22042292b162906ff08d9171681d25236d7b7100
Prabash <pd.prabashmadusanka@gmail.com>
2019-11-09 00:43:40 +0530

    Added Mule project

--- SQL/StaffMgt.sql
+++ SQL/StaffMgt.sql
@@ -25,8 +25,19 @@ create table StaffMember(
 	Isdeleted boolean,
 	IsActive boolean,
 	LogInfo  varchar(20),
-	LibraryId int,
+	LibraryId varchar(50),
 	DesignationID int,
 	constraint Staffpk primary key(StaffID),
 	constraint fk_DesignationID FOREIGN KEY (DesignationID) references Designation(DesignationID)
 )
+
+
+DELIMITER $$
+DROP PROCEDURE IF EXISTS getStaff $$
+CREATE PROCEDURE getStaff(IN parmLibraryId varchar(50))
+BEGIN
+	select SM.StaffID,SM.StaffName,SM.EmpNo,SM.JoinDate,SM.Emailaddress,SM.Address,D.DesignationName from StaffMember SM, Designation D  where SM.LibraryId= parmLibraryId and D.DesignationID= SM.DesignationID and  SM.Isdeleted=0 and SM.IsActive=1;
+	
+END $$
+
+DELIMITER ;
